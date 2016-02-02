f72af7af8ae9530b9c1965e1585ad7538fe74ab1
bigchiefmaiz <alexkorn422@aol.com>
2016-02-02 15:43:41 -0500

    Update filter.py

--- filter.py
+++ filter.py
@@ -1,2 +1,10 @@
 #!/usr/bin/python
 
+#import sqlite3
+#
+#conn= sqlite3.connect("Henry's Database")
+#cur = con.cursor(15)
+#cur.execute ("SELECT `spamicity` FROM `Henry's Database` WHERE `spamicity`>=.85")
+# And then some query where we will take the highest spamicity words in the     message
+
+
