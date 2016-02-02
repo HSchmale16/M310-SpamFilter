e0ce35e02766c389cfe919334fd8731753fa05db
bigchiefmaiz <alexkorn422@aol.com>
2016-02-02 16:51:25 -0500

    Update spamParse.py

--- spamParse.py
+++ spamParse.py
@@ -1,1 +1,11 @@
 # Contains the spam parsing functions
+import email 
+
+msg = email.message
+
+from cStringIO import StringIO
+from email.generator import Generator
+fp = StringIO()
+g = Generator(fp,mangle_from_=Fasle, maxheaderlen=60)
+g.flatten(msg)
+text = fp.getvalue()
